1ba01629e8bbc79ca9eafd3f1ed0a3b39a771b54
hmcka <hethur@hmcka.com>
2018-08-26 14:07:09 -0400

    Can play 3 times or until you win.

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,23 +3,39 @@ import java.util.Scanner;
 public class GuessingGame {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		int secretNumber = 5;
+		int secretNumber = (int) Math.ceil(Math.random() * 10);
+		int attempt = 0;
 		
+		System.out.println(secretNumber);
+
 		System.out.println("Welcome to our guessing game.");
 		System.out.println("Pick a number between 1 and 10:");
 		int guessNumber = input.nextInt();
-		
-		String result;
-		if(secretNumber==guessNumber) {
-			result = "You won!";
-		} else {
-			result = "You lost!";
+		attempt++;
+
+		while (attempt < 3) {
+			String result;
+			if (secretNumber == guessNumber) {
+				result = "You won!";
+				attempt = 3;
+				System.out.println("You won.");
+			} else {
+				result = "Try again";
+				System.out.println("Pick a number between 1 and 10:");
+				guessNumber = input.nextInt();
+				attempt++;
+				if (attempt == 3) {
+					System.out.println("Game Over.");
+				}
+			}
+			
+			//System.out.println(result);
 		}
+
 		
-		System.out.println(result);
-		
+
 		input.close();
 
 	}
